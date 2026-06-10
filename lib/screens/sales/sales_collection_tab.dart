50c1a26b4914b0df53c22ad17e3a954a2f5afbed
ssatrya717@gmail.com <Satrya>
2026-06-10 14:12:27 +0700

    fix remake desain tabs active and not active

--- lib/screens/sales/sales_collection_tab.dart
+++ lib/screens/sales/sales_collection_tab.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../theme/app_colors.dart';
 import 'collection/ar_aging_tab.dart';
 import 'collection/outstanding_invoice_tab.dart';
 import 'collection/promise_to_pay_tab.dart';
@@ -8,30 +9,36 @@ class SalesCollectionTab extends StatelessWidget {
   const SalesCollectionTab({super.key});
 
   @override
-  Widget build(BuildContext context) => const DefaultTabController(
-    length: 3,
-    child: Column(
-      children: [
-        Material(
-          child: TabBar(
-            isScrollable: true,
-            tabs: [
-              Tab(text: 'AR Aging & Rank'),
-              Tab(text: 'Overdue & Invoices'),
-              Tab(text: 'Janji Bayar'),
-            ],
-          ),
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: ColoredBox(
+        color: AppColors.background,
+        child: Column(
+          children: [
+            Material(
+              color: AppColors.white,
+              elevation: 1,
+              child: TabBar(
+                tabs: const [
+                  Tab(text: 'AR Aging'),
+                  Tab(text: 'Invoice'),
+                  Tab(text: 'Janji Bayar'),
+                ],
+              ),
+            ),
+            const Expanded(
+              child: TabBarView(
+                children: [
+                  ArAgingTab(),
+                  OutstandingInvoiceTab(),
+                  PromiseToPayTab(),
+                ],
+              ),
+            ),
+          ],
         ),
-        Expanded(
-          child: TabBarView(
-            children: [
-              ArAgingTab(),
-              OutstandingInvoiceTab(),
-              PromiseToPayTab(),
-            ],
-          ),
-        ),
-      ],
-    ),
-  );
+      ),
+    );
+  }
 }
