c0ece47f34742c96dc320f418dfeeef54dc89c1f
ssatrya717@gmail.com <Satrya>
2026-08-18 10:52:40 +0700

    fix link url update aplikasi

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -20,7 +20,7 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen>
     with SingleTickerProviderStateMixin {
   static final Uri _appUpdateUri = Uri.parse(
-    'https://play.google.com/apps/testing/com.tmsxhub',
+    'https://play.google.com/store/apps/details?id=com.tmsxhub&hl=en-US&ah=e76XkbMCe5OQdYkzEWSgI5nwdOQ',
   );
   static const Color _accentTeal = Color(0xFF14B8A6);
   static const Color _accentBlue = Color(0xFF3B82F6);
